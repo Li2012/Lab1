bfaed2c0867a21397378f56b9ccd1aac3495b315
yl25 <linfeixb27@gmail.com>
2011-10-14 13:04:20 -0500

    Added Const.h file

---
+++ Server/Const.h
@@ -0,0 +1,15 @@
+//Const.h
+
+#ifndef _CONST_H_
+#define _CONST_H_
+
+const int default_port = 11111;
+const int string_length = 200;
+
+const std::string dealer_name_key = "DN";
+const std::string games_key = "GM";
+const std::string delimiter = "~$";
+const std::string player_name_key = "PN";
+
+
+#endif
